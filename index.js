ccd83176d7e37fa8b472769cf1f30e647289d128
rootKid <137388933+devMainak@users.noreply.github.com>
2024-06-28 09:22:15 +0000

    feat: POST-"/categories"

--- index.js
+++ index.js
@@ -10,7 +10,35 @@ initializeDatabse();
 // JSON parsing middleware implemented
 app.use(express.json());
 
-//
+// Function to seed books in DB 
+const seedCategories = async (category) => {
+  try {
+    const categoryToSave = new BooksCategories(category);
+    const savedCategory = await categoryToSave.save();
+    return savedCategory
+  } catch (error) {
+    throw error;
+  }
+}
+
+// POST method on "/categories" to seed categories in DB
+app.post("/categories", async (req, res) => {
+ try {
+   const savedCategory = await seedCategories(req.body)
+   if (savedCategory)
+   {
+     res.status(201)
+     .json({message: "Category saved successfully.", savedCategory: savedCategory})
+   } else {
+     res.status(400)
+     .json({error: "Some error occured while saving new category."})
+   }
+ } catch (err) {
+   console.error(err)
+   res.status(500)
+   .json({error: "Failed to save new category"})
+ }
+})
 
 // Function to seed categories in DB
 const seedBooks = async () => {
@@ -25,9 +53,9 @@ const seedBooks = async () => {
 
 seedBooks();
 
-// // Listiening to the port for HTTP requests
-// const PORT = 3000
+// Listiening to the port for HTTP requests
+const PORT = 3000
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
