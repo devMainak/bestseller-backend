0564bf5b832ca38943b854a656afabb7320981ce
bitMan <mrterm404@gmail.com>
2024-11-04 16:00:52 +0530

    feat: schema updated (model referencing added)

--- models/wishlist.model.js
+++ models/wishlist.model.js
@@ -2,38 +2,7 @@ const mongoose = require('mongoose')
 
 // Defining wishlist mongoose schema
 const wishlistSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  author: {
-    type: String,
-    required: true
-  },
-  publishedYear: {
-    type: Number,
-    required: true
-  },
-  categoryName: {
-    type: String,
-    enum: ["Fiction", "Non-Fiction", "Psychology", "Self-Help", "Business"]
-  },
-  language: {
-    type: String,
-    default: "English"
-  },
-  country: {
-    type: String,
-    default: "United States"
-  },
-  price: Number,
-  rating: Number,
-  summary: String,
-  isDeliveryFree: {
-    type: String,
-    default: true
-  },
-  coverImageUrl: String
+  book: { type: mongoose.Schema.Types.ObjectId, ref: "BooksDatas" }
 })
 
 // Defining mongoose model for wishlist
